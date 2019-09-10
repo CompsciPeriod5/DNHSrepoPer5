5bb5755e8a5a30eacd825f972301152413294979
Michael Hayes <mhayescs19@gmail.com>
2019-09-10 15:20:57 -0700

    Initial Commit

---
+++ struct/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  struct
+//
+//  Created by Hayes, Michael on 9/10/19.
+//  Copyright © 2019 Hayes, Michael. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

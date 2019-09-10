d22d5033dd29a2b792a599927aedf3636bc83410
Michael Hayes <mhayescs19@gmail.com>
2019-09-10 15:29:03 -0700

    from structures team

---
+++ struct/structQuestion.h
@@ -0,0 +1,14 @@
+//
+//  structQuestion.h
+//  struct
+//
+//  Created by Hayes, Michael on 9/10/19.
+//  Copyright © 2019 Hayes, Michael. All rights reserved.
+//
+
+#ifndef structQuestion_h
+#define structQuestion_h
+
+#include <stdio.h>
+int Question(void);
+#endif /* structQuestion_h */

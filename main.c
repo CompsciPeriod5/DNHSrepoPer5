4a3f76f53cf3edda8202dcab0dced4aef93ba0b5
nakulnandhakumar <55005060+nakulnandhakumar@users.noreply.github.com>
2019-09-10 15:25:58 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,10 @@
-// Teacher was here
+// Starting main.c
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    // This is a commit
+    // Push
+    printf("Hello, World!\n");
+    return 0;
+}

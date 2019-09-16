8c704eec369b84b61016dc828fe7593338cf93af
Brent Arcinue <brentarcinuegithub@gmail.com>
2019-09-16 14:47:10 -0700

    Initial Commit

---
+++ IO Midterm Test Question/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  IO Midterm Test Question
+//
+//  Created by Arcinue, Brent Gregory on 9/16/19.
+//  Copyright © 2019 Simple IO. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

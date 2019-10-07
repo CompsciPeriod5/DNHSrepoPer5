0d608f848fa69d3c43ce6d8ee98d59ab86f2e8f0
Tanmay Marwah <marwah.tanmay@gmail.com>
2019-10-07 14:31:31 -0700

    Fixed Formatting

--- MidTerm/operatorRLQuestions.c
+++ MidTerm/operatorRLQuestions.c
@@ -13,36 +13,36 @@ int opQuestions(void){
     printf("The Expression is 3+=5\n");
     printf("A is the left term\n");
     printf("What is the new value for 'A' after the expression is run?\n");
-    printf("A) 3 B) 6 C) -2 D) 8");
+    printf("A) 3 B) 6 C) -2 D) 8\n");
     scanf("%c",&ans1);
     if(ans1 == 'd'){
-        printf("Correct! Loading Next question...");
+        printf("Correct!\n");
     } else{
-        printf("Incorrect. The correct answer is D");
+        printf("Incorrect. The correct answer is D\n");
     }
     
     printf("Please answer all questions in lowercase!\n");
     printf("The Expression is 7+=5\n");
     printf("A is the left term\n");
     printf("What is the new value for 'A' after the expression is run?\n");
-    printf("A) 2 B) 3 C) 12 D) 5");
+    printf("A) 2 B) 3 C) 12 D) 5\n");
     scanf("%c",&ans2);
     if(ans2 == 'c'){
-        printf("Correct! Loading Next question...");
+        printf("Correct! \n");
     } else{
-        printf("Incorrect. The correct answer is C");
+        printf("Incorrect. The correct answer is C\n");
     }
     
     printf("Please answer all questions in lowercase!\n");
     printf("The Expression is 12/=2\n");
     printf("A is the left term\n");
     printf("What is the new value for 'A' after the expression is run?\n");
-    printf("A) 9 B) 6 C) 24 D) 10");
+    printf("A) 9 B) 6 C) 24 D) 10\n");
     scanf("%c",&ans3);
     if(ans3 == 'b'){
-        printf("Correct! Loading Next question...");
+        printf("Correct!\n");
     } else{
-        printf("Incorrect. The correct answer is D");
+        printf("Incorrect. The correct answer is D\n");
     }
     return 0;
 }
